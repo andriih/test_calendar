3cee93988e19e2e07a7bcb1e9497d0db5d4dd41b
andriih <andrii.hnatyshyn@gmail.com>
2017-04-11 16:42:14 +0300

    problem with output

--- app/app.js
+++ app/app.js
@@ -1,26 +1,31 @@
+var formYear = $('#year').val();
+var formMonth = $('#month').val();
+
 function createCalendar(id, year, month){
 	
 	var destLocation = document.getElementById(id);
-	var formYear = $('#year').val();
-	var formMonth = $('#month').val();
 	var days  = new Date(formYear, formMonth, 0).getDate();
+	var table = $(destLocation).append('<table></table>').addClass('table table-bordered table striped');	
+	
+	for(var i = 1; i<=days;i++){
+		
+		for (var j =1; j<=7;j++){
+			var tr = $(table).append('<tr><td>'+i+'</td></tr>');
+		}
 
-	// var days  = new Date(formYear, formMonth, 0).getDate();
-	 $(destLocation).append(days);
-	//console.log(days);
+		
+
+		
+	}
+
+	 
+	//console.log(table);
 
 };
 
 $('#calendar-form').submit(function(e){
 	e.preventDefault();
-	//	var destLocation = document.getElementById(id);
-	// var formYear = $('#year').val();
-	// var formMonth = $('#month').val();
-	// var days  = new Date(formYear, formMonth, 0).getDate();
 
-
- // 	console.log(days);
-
- createCalendar("calendar__wrapper");
+	createCalendar("calendar__wrapper");
 	
 });
